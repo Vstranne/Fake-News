cbfaeff475d73d12bc4f53bad750935b40a6cd5f
Vstranne <victorn87@gmail.com>
2020-10-20 09:00:25 +0200

    added a few files and some code

---
+++ data.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+$newsArticles = [
+    [
+        'author' => 'Author Authorsson',
+        'title' => 'News Title',
+        'article' => 'Lorem Ipsum',
+        'date' => 2020-01-01
+    ],
+    [
+        'author' => 'Author Authorsson',
+        'title' => 'News Title',
+        'article' => 'Lorem Ipsum',
+        'date' => 2020-01-01  
+    ],
+    [
+        'author' => 'Author Authorsson',
+        'title' => 'News Title',
+        'article' => 'Lorem Ipsum',
+        'date' => 2020-01-01  
+    ],
+    [
+        'author' => 'Author Authorsson',
+        'title' => 'News Title',
+        'article' => 'Lorem Ipsum',
+        'date' => 2020-01-01  
+    ]
+];
+?>
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+require __DIR__ . '/data.php';
+require __DIR__ . '/functions.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +10,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fake News!</title>
     <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
-    
+    <header>
+
+    </header>
+    <?php foreach($newsArticles as $newsArticle) : ?>
+        <div class="news">
+            <p><?= $newsArticle['title'] ?></p>
+        </div>
+        <br>
+    <?php endforeach; ?>
+    <footer>
+
+    </footer>
 </body>
 </html>
