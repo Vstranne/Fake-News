1e012812118fb95206a553560ac74ec2c75fde9b
Vstranne <victorn87@gmail.com>
2020-10-23 15:23:03 +0200

    small updates

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require __DIR__ . '/functions.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fake News!</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Russo+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 
 </head>
@@ -16,12 +18,19 @@ require __DIR__ . '/functions.php';
     <header>
 
     </header>
-    <?php foreach($newsArticles as $newsArticle) : ?>
-        <div class="news">
-            <p><?= $newsArticle['title'] ?></p>
-        </div>
-        <br>
-    <?php endforeach; ?>
+    <main>
+        <?php foreach($newsArticles as $newsArticle) : ?>
+            <div class="news">
+                <h2 class="newsTitle"><?= $newsArticle['title']; ?></h2>
+                <p class="newsItem"><?= $newsArticle['article']; ?></p>
+                <div class="newsBottom">
+                    <p> <?= newsArticle['author']; ?></p>
+                    <p> <?= newsArticle['date']; ?></p>
+                    <p> <?= newsArticle['likes']; ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </main>
     <footer>
 
     </footer>
