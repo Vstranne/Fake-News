a2953d2792d372528021364e5afd2215d3c626a8
Vstranne <victorn87@gmail.com>
2020-10-28 20:17:05 +0100

    some small additions

--- data.php
+++ data.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $newsArticles = [
     [
         'author' => 2,
-        'title' => 'News Title',
+        'title' => "I gotta piss",
         'article' => "Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.",
         'date' => '2019-01-01',
         'likes' => '10',
@@ -13,7 +13,7 @@ $newsArticles = [
     ],
     [
         'author' => 3,
-        'title' => 'News Title',
+        'title' => "We happy?",
         'article' => "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
         'date' => '2020-01-01',
         'likes' => '24',
@@ -21,7 +21,7 @@ $newsArticles = [
     ],
     [
         'author' => 0,
-        'title' => 'News Title',
+        'title' => "I'm serious as a heart attack",
         'article' => "My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?",
         'date' => '2020-02-01' ,
         'likes' => '25',
@@ -29,7 +29,7 @@ $newsArticles = [
     ],
     [
         'author' => 1,
-        'title' => 'News Title',
+        'title' => "Are you ready for the truth?",
         'article' => "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
         'date' => '2020-02-25',
         'likes' => '12',
