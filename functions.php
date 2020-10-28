0f912231cbe1add3e51dcf7b466b3ed579df88b6
Victor Stranne <victorn87@gmail.com>
2020-10-28 23:54:59 +0100

    added function to that can help determine how long ago an article was posted

--- functions.php
+++ functions.php
@@ -10,5 +10,48 @@ function sortByDate( $a, $b )
     return strtotime($b["date"]) - strtotime($a["date"]);
 }
 
+function timeSinceNow (string $date) : array
+{
+    // If input is a future date
+    if (strtotime("now") < strtotime($date)){
+        $timeElapsed = [
+            'ErrorMessage' => "The input date is in the future you goon!"
+        ];
+        return $timeElapsed;
+    }
+    
+    // Time difference in seconds from today
+    $timeDifference = strtotime("now") - strtotime($date);
+    
+    // Amounts of years
+    $years = intdiv($timeDifference, (60 * 60 * 24 * 365));
+    if ($years > 0) 
+        $timeDifference = $timeDifference - ($years * 60 * 60 * 24 * 365);
 
+    // Amount of days
+    $days = intdiv($timeDifference, (60 * 60 * 24));
+        
+    if ($days > 0)
+        $timeDifference = $timeDifference - ($days * 60 * 60 * 24);
+
+    // Amount of hours
+    $hours = intdiv($timeDifference, (60 * 60));
+    
+    if ($hours > 0)
+        $timeDifference = $timeDifference - ($hours * 60 * 60);
+
+    // Amount of minutes
+    $minutes = intdiv($timeDifference, 60);
+
+    $timeElapsed = [
+        'years' => $years,
+        'days' => $days,
+        'hours' => $hours,
+        'minutes' => $minutes
+    ];
+    return $timeElapsed;
+}
+// $testvalue = "last sunday";
+// $test = timeSinceNow($testvalue);
+// print_r($test);
 ?>
